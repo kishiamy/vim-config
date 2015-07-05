f30f60e2d96fb80ffc69c086f1e21edbf7d5987e
Juan Fraire <groteck@gmail.com>
2015-07-05 12:39:08 +0200

    install vim plug before install my vim config

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-wget -O - https://raw.githubusercontent.com/groteck/vim-conf/master/install.sh | bash
+wget -O - https://raw.githubusercontent.com/groteck/vim-config/master/install.sh | bash
--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 VIMRC_FILE=https://raw.githubusercontent.com/groteck/vim/vim-plug/vimrc
 VIM_PLUGINS_FILE=https://raw.githubusercontent.com/groteck/vim/vim-plug/bundles.vim
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
 mkdir .vim .vim/bundle .vim/backup .vim/swap .vim/cache .vim/undo &&
 curl -o ~/.vim/bundles.vim  $VIM_PLUGINS_FILE &&
 curl -o ~/.vimrc $VIMRC_FILE &&
