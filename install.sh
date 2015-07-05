dfb940873830ae16862a2afbdb185617c01d0586
Juan Fraire <groteck@gmail.com>
2015-07-05 12:47:07 +0200

    create directories before install vim plug

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 VIMRC_FILE=https://raw.githubusercontent.com/groteck/vim/vim-plug/vimrc
 VIM_PLUGINS_FILE=https://raw.githubusercontent.com/groteck/vim/vim-plug/bundles.vim
 
+mkdir .vim .vim/bundle .vim/backup .vim/swap .vim/cache .vim/undo &&
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
-mkdir .vim .vim/bundle .vim/backup .vim/swap .vim/cache .vim/undo &&
 curl -o ~/.vim/bundles.vim  $VIM_PLUGINS_FILE &&
 curl -o ~/.vimrc $VIMRC_FILE &&
 vim -N -u ~/.vim/bundles.vim +PlugInstall +qall
